b76d15606e5715a8e6ce10b022f34dc551d8a076
PCY <pcx474@gmail.com>
2020-05-21 02:12:18 +0900

    update
    db

--- server/Server.js
+++ server/Server.js
@@ -28,7 +28,14 @@ app.use('/api', (req, res) => {
     'INSERT INTO fintech.user (name, email, password, accesstoken, refreshtoken, userseqno) VALUES (?,?,?,?,?,?)';
   connection.query(
     sql, // excute sql
-    ['bryan', '@@@@@', '21312', 'access', 'refresh', 'seqence'], // ? <- value
+    [
+      req.body.userName,
+      req.body.email,
+      req.body.password,
+      req.body.accesstoken,
+      req.body.refreshtoken,
+      req.body.userseqno,
+    ], // ? <- value
     function (err, result) {
       if (err) {
         console.error(err);
@@ -39,6 +46,35 @@ app.use('/api', (req, res) => {
       }
     }
   );
+});
+
+app.get('/authResult', function (req, res) {
+  var authCode = req.query.code;
+  console.log(authCode);
+  var option = {
+    method: 'POST',
+    url: 'https://testapi.openbanking.or.kr/oauth/2.0/token',
+    header: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    form: {
+      code: authCode,
+      client_id: 'HxTaO0dyeVPIepwel60gaJT2uCwCod8dwbWGH24m',
+      client_secret: 'XFoOqPP7IUOaW0H9VGwcdjtYZa25mc5KKl1yoeKC',
+      redirect_uri: 'http://localhost:3002/authResult',
+      grant_type: 'authorization_code',
+    },
+  };
+  request(option, function (err, response, body) {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      var accessRequestResult = JSON.parse(body);
+      console.log(accessRequestResult);
+      res.render('resultChild', { data: accessRequestResult });
+    }
+  });
 });
 
 app.listen(port, () => {
